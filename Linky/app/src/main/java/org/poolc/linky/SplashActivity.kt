27a31f3a366a96112bfd1d776fde989450939e4e
gloria <ippnsj@gmail.com>
2022-08-02 16:51:52 +0900

    Start project

---
+++ Linky/app/src/main/java/org/poolc/linky/SplashActivity.kt
@@ -0,0 +1,25 @@
+package org.poolc.linky
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import org.poolc.linky.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+    }
+}
