bbb0c0667e4470938c5bb247ed25a790713741c6
gloria <ippnsj@gmail.com>
2022-08-08 21:23:17 +0900

    Add Add Linky Activity

---
+++ Linky/app/src/main/java/org/poolc/linky/AddLinkyActivity.kt
@@ -0,0 +1,122 @@
+package org.poolc.linky
+
+import android.content.Intent
+import android.graphics.BitmapFactory
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import org.jsoup.Jsoup
+import org.poolc.linky.databinding.ActivityAddLinkyBinding
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.regex.Pattern
+import kotlin.concurrent.thread
+
+class AddLinkyActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityAddLinkyBinding
+    private var folders = arrayOf("선택안함", "음식", "고양이", "직접입력")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddLinkyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            // topbar 설정
+            setSupportActionBar(addLinkyTopbar)
+            supportActionBar?.setDisplayShowTitleEnabled(false)
+            addLinkyTopbarTitle.text = "링크 추가하기"
+
+            // spinner 설정
+            val spinnerAdapter = ArrayAdapter(this@AddLinkyActivity, android.R.layout.simple_spinner_item, folders)
+            spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            folderSpinner.adapter = spinnerAdapter
+            folderSpinner.onItemSelectedListener = spinnerListener
+            folderTextInput.isEnabled = false
+
+            // 공유하기로부터 온 intent 처리
+            if(intent.action == Intent.ACTION_SEND && intent.type != null) {
+                if(intent.type == "text/plain") {
+                    val txt = intent.getStringExtra(Intent.EXTRA_TEXT).toString()
+                    val pattern = Pattern.compile("((https?|ftp|gopher|telnet|file):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)", Pattern.CASE_INSENSITIVE)
+                    val matcher = pattern.matcher(txt)
+                    if(matcher.find()) {
+                        val url = txt.substring(matcher.start(0), matcher.end(0))
+
+                        // 메타데이터 추출
+                        thread {
+                            val doc = Jsoup.connect(url).get()
+                            val title =
+                                doc.select("meta[property=og:title]").first()?.attr("content");
+                            val image =
+                                doc.select("meta[property=og:image]").get(0).attr("content")
+                            Log.d("test-title", title!!)
+                            Log.d("test-imageUrl", image)
+
+                            val imageUrl = URL(image)
+                            val conn = imageUrl.openConnection() as HttpURLConnection
+                            val bitmap = BitmapFactory.decodeStream(conn.inputStream)
+
+                            runOnUiThread {
+                                // 링크주소
+                                linkAddressTextInput.setText(url)
+                                linkAddressTextInput.isEnabled = false
+
+                                // 제목
+                                titleTextInput.setText(title ?: "")
+
+                                // 대표이미지
+                                if(bitmap != null) {
+                                    linkImage.setImageBitmap(bitmap)
+                                }
+                            }
+                        }
+                    }
+                    else {
+                        Log.d("test", "NO URL")
+                    }
+                }
+            }
+        }
+    }
+
+    val spinnerListener = object : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            when(parent?.id) {
+                R.id.folderSpinner -> {
+                    if(position == folders.size - 1) {
+                        binding.folderTextInput.setText("")
+                        binding.folderTextInput.isEnabled = true
+                    }
+                    else {
+                        binding.folderTextInput.setText(folders[position])
+                        binding.folderTextInput.isEnabled = false
+                    }
+                }
+            }
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_add_linky_topbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId) {
+            R.id.add_link -> {
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
--- Linky/app/src/main/java/org/poolc/linky/MainActivity.kt
+++ Linky/app/src/main/java/org/poolc/linky/MainActivity.kt
@@ -1,13 +1,16 @@
 package org.poolc.linky
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import org.jsoup.Jsoup
 import org.poolc.linky.databinding.ActivityMainBinding
+import java.net.MalformedURLException
+import java.net.URISyntaxException
+import java.net.URL
 import java.util.regex.Pattern
 import kotlin.concurrent.thread
 
@@ -19,7 +22,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
 
         with(binding) {
             // topbar 설정
@@ -34,15 +36,19 @@ class MainActivity : AppCompatActivity() {
                 changeFragment(
                     when(item.itemId) {
                         R.id.linky -> {
+                            topbarTitle.text = "내 링키"
                             LinkyFragment()
                         }
                         R.id.search -> {
+                            topbarTitle.text = "둘러보기"
                             SearchFragment()
                         }
                         R.id.more -> {
+                            topbarTitle.text = "설정"
                             MoreFragment()
                         }
                         else -> {
+                            topbarTitle.text = "내 링키"
                             LinkyFragment()
                         }
                     }
@@ -50,34 +56,6 @@ class MainActivity : AppCompatActivity() {
                 true
             }
             bottomNavigation.selectedItemId = R.id.linky
-
-            // 공유하기로부터 온 intent 처리
-            if(intent.action == Intent.ACTION_SEND && intent.type != null) {
-                if(intent.type == "text/plain") {
-                    val txt = intent.getStringExtra(Intent.EXTRA_TEXT).toString()
-                    val pattern = Pattern.compile("((https?|ftp|gopher|telnet|file):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)", Pattern.CASE_INSENSITIVE)
-                    val matcher = pattern.matcher(txt)
-                    if(matcher.find()) {
-                        val url = txt.substring(matcher.start(0), matcher.end(0))
-                        Log.d("test1", txt)
-                        Log.d("test2", url)
-
-                        // 메타데이터 추출
-                        thread {
-                            val doc = Jsoup.connect(url).get()
-                            val title = doc.select("meta[property=og:title]").first()?.attr("content");
-                            val description = doc.select("meta[property=og:description]").get(0).attr("content")
-                            val imageUrl = doc.select("meta[property=og:image]").get(0).attr("content")
-                            Log.d("test-title", title!!)
-                            Log.d("test-description", description)
-                            Log.d("test-imageUrl", imageUrl)
-                        }
-                    }
-                    else {
-                        Log.d("test3", "NO URL")
-                    }
-                }
-            }
         }
     }
 
